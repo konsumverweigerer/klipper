a08cb8a79ef41cb1d6de919bc944b0de40d83fcb
Peter Gruber <gruberp@googlemail.com>
2021-02-05 23:45:29 +0100

    improve mixing

--- klippy/extras/mixingextruder.py
+++ klippy/extras/mixingextruder.py
@@ -7,6 +7,22 @@ import math, logging
 import stepper, chelper
 
 from toolhead import Move
+from gcode import GCodeCommand
+
+class MixingMove:
+    def __init__(self, x, y, z, e,
+                 dist_x, dist_y, dist_z, dist_e,
+                 ratio_x, ratio_y, ratio_z, ratio_e,
+                 distance, acceleration,
+                 velocity_start, velocity_cruise,
+                 time_accel, time_cruise, time_decel):
+        self.axes_d = (dist_x, dist_y, dist_z, dist_e)
+        self.axes_r = (ratio_x, ratio_y, ratio_z, ratio_e)
+        self.move_d = distance
+        self.accel = acceleration
+        self.start_v, self.cruise_v = velocity_start, velocity_cruise
+        self.accel_t, self.cruise_t, self.decel_t = time_accel, time_cruise, time_decel
+        self.start_pos = (x, y, z, e)
 
 class MixingExtruder:
     def __init__(self, config):
@@ -19,7 +35,8 @@ class MixingExtruder:
         self.main_extruder = extruders[0]
         pheaters = self.printer.load_object(config, 'heaters')
         self.heater = pheaters.lookup_heater(self.main_extruder)
-        self.extruders = [self.printer.lookup_object(extruder) for extruder in extruders]
+        self.extruders = [self.printer.lookup_object(extruder)
+                          for extruder in extruders]
         self.mixing = [0 if p else 1 for p in range(len(self.extruders))]
         self.positions = [0. for p in range(len(self.extruders))]
         self.ratios = [0 for p in range(len(self.extruders))]
@@ -32,6 +49,8 @@ class MixingExtruder:
             gcode.register_command("M163", self.cmd_M163)
             gcode.register_command("M164", self.cmd_M164)
             gcode.register_command("M567", self.cmd_M567)
+            self.orig_G1 = gcode.register_command("G1", None)
+            gcode.register_command("G1", self.cmd_G1)
         gcode.register_mux_command("ACTIVATE_EXTRUDER", "EXTRUDER",
                                    self.name, self.cmd_ACTIVATE_EXTRUDER,
                                    desc=self.cmd_ACTIVATE_EXTRUDER_help)
@@ -46,13 +65,13 @@ class MixingExtruder:
         return move.max_cruise_v2
     def _scale_move(self, move, idx):
         mixing = self.mixing[idx]
-        extrude_start_pos = self.positions[idx]
-        extrude_end_pos = extrude_start_pos + mixing * (move.end_pos[3] - move.start_pos[3])
-        # TODO: create reduced MixingMove
-        return Move(self.printer.lookup_object('toolhead'),
-                    move.start_pos[0:3] + (extrude_start_pos,),
-                    move.end_pos[:3] + (extrude_end_pos,),
-                    (move.move_d / move.min_move_t) if move.is_kenimatic_move else (mixing * move.move_d / move.min_move_t))
+        if not mixing:
+            return None
+        return MixingMove(move.start_pos[0], move.start_pos[1], move.start_pos[2], self.positions[idx],
+                          move.axes_d[0], move.axes_d[1], move.axes_d[2], mixing * move.axes_d[3],
+                          move.axes_r[0], move.axes_r[1], move.axes_r[2], mixing * move.axes_r[3],
+                          move.move_d, move.accel, move.start_v, move.cruise_v,
+                          move.accel_t, move.cruise_t, move.decel_t)
     def _check_move(self, scaled_move, move):
         axis_r = scaled_move.axes_r[3]
         axis_d = scaled_move.axes_d[3]
@@ -62,34 +81,36 @@ class MixingExtruder:
                 "See the 'min_extrude_temp' config option for details")
         if (not move.axes_d[0] and not move.axes_d[1]) or axis_r < 0.:
             # Extrude only move (or retraction move) - limit accel and velocity
-            if abs(axis_d) > self.max_e_dist:
+            if abs(axis_d) > self.main_extruder.max_e_dist:
                 raise self.printer.command_error(
                     "Extrude only move too long (%.3fmm vs %.3fmm)\n"
                     "See the 'max_extrude_only_distance' config"
                     " option for details" % (axis_d, self.max_e_dist))
             inv_extrude_r = 1. / abs(axis_r)
-            move.limit_speed(self.max_e_velocity * inv_extrude_r,
-                             self.max_e_accel * inv_extrude_r)
-        elif axis_r > self.max_extrude_ratio:
-            if axis_d <= self.nozzle_diameter * self.max_extrude_ratio:
+            move.limit_speed(self.main_extruder.max_e_velocity * inv_extrude_r,
+                             self.main_extruder.max_e_accel * inv_extrude_r)
+        elif axis_r > self.main_extruder.max_extrude_ratio:
+            if axis_d <= self.main_extruder.nozzle_diameter * self.main_extruder.max_extrude_ratio:
                 # Permit extrusion if amount extruded is tiny
                 return
-            area = axis_r * self.filament_area
+            area = axis_r * self.main_extruder.filament_area
             logging.debug("Overextrude: %s vs %s (area=%.3f dist=%.3f)",
-                          axis_r, self.max_extrude_ratio, area, move.move_d)
+                          axis_r, self.main_extruder.max_extrude_ratio, area, move.move_d)
             raise self.printer.command_error(
                 "Move exceeds maximum extrusion (%.3fmm^2 vs %.3fmm^2)\n"
                 "See the 'max_extrude_cross_section' config option for details"
-                % (area, self.max_extrude_ratio * self.filament_area))
+                % (area, self.main_extruder.max_extrude_ratio * self.main_extruder.filament_area))
     def check_move(self, move):
         for idx, extruder in enumerate(self.extruders):
             scaled_move = self._scale_move(move, idx)
-            self._check_move(scaled_move, move)
+            if scaled_move:
+                self._check_move(scaled_move, move)
     def move(self, print_time, move):
         for idx, extruder in enumerate(self.extruders):
             scaled_move = self._scale_move(move, idx)
-            extruder.move(print_time, scaled_move)
-            self.positions[idx] = scaled_move.end_pos[3]
+            if scaled_move:
+                extruder.move(print_time, scaled_move)
+                self.positions[idx] = scaled_move.end_pos[3]
     def get_status(self, eventtime):
         return dict(self.stats(eventtime),
                     ticks=", ".join(extruder.stepper.get_mcu_position() for extruder in self.extruders)
@@ -105,20 +126,53 @@ class MixingExtruder:
         weight = gcmd.get_float('P', 0., minval=0.)
         self.ratios[index] = weight
     def cmd_M164(self, gcmd):
-        index = gcmd.get_int('S', None, minval=0, maxval=len(self.extruders))
+        mixingextruder = self
+        index = gcmd.get_int('S', 0, minval=0, maxval=len(self.extruders))
+        if index:
+            mixingextruder = self.printer.lookup_object(
+                "mixingextruder%d" % (index))
+            if not mixingextruder:
+                raise gcmd.error("Invalid extruder index")
         s = sum(self.ratios)
         if s <= 0:
             raise gcmd.error("Could not save ratio: its empty")
         for i, v in enumerate(self.ratios):
-            self.mixing[i] = v/s
+            mixingextruder.mixing[i] = v/s
     def cmd_M567(self, gcmd):
+        mixingextruder = self
         index = gcmd.get_int('P', 0, minval=0, maxval=len(self.extruders))
+        if index:
+            mixingextruder = self.printer.lookup_object(
+                "mixingextruder%d" % (index))
+            if not mixingextruder:
+                raise gcmd.error("Invalid extruder index")
         weighting = gcmd.get('E', None)
-        s = sum(weighting)
+        if not weighting:
+            raise gcmd.error("No ratio in M567")
+        weights = [float(w) for w in weighting.split(":")]
+        if min(weights) < 0:
+            raise gcmd.error("Negative weight not allowed")
+        s = sum(weights)
         if not 0 <= s < 1:
             raise gcmd.error("Could not save ratio: its empty")
-        for i, v in enumerate(weightig):
-            self.mixing[i] = v/s
+        for i, v in enumerate(weights):
+            mixingextruder.mixing[i] = v/s
+    def cmd_G1(self, gcmd):
+        gcode = self.printer.lookup_object('gcode')
+        weighting = gcmd.get('E', None)
+        if not weighting or ":" not in weighting:
+            self.orig_G1(gcmd)
+        weights = [float(w) for w in weighting.split(":")]
+        if min(weights) < 0:
+            raise gcmd.error("Negative weight not allowed")
+        extrude = sum(weights)
+        self.cmd_M567(GCodeCommand(
+            gcode, "M567", "M567 E%s" % (weighting),
+            dict(E=weighting), gcmd._need_ack))
+        self.orig_G1(GCodeCommand(
+            gcode, "G1", gcmd.get_commandline(),
+            dict(gcmd.get_command_parameters(), E="%f" % (extrude)),
+            gcmd._need_ack))
     cmd_ACTIVATE_EXTRUDER_help = "Change the active extruder"
     def cmd_ACTIVATE_EXTRUDER(self, gcmd):
         toolhead = self.printer.lookup_object('toolhead')
